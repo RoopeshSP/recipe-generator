1b91c8b8f5c1253e9a5017b5120f1252358ffa6a
Naga Rithesh <nagarithesh@intergrity.co>
2025-10-16 22:55:04 +0530

    minor fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+
 import { Providers } from '../components/providers'
 import { Navigation } from '../components/navigation'
 
